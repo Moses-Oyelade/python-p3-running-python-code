7915228f0d6a1feca26eb315bf2bebffd770a84c
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-27 00:37:22 +0100

    all 3 tests paased

--- lib/app.py
+++ lib/app.py
@@ -1,5 +1,4 @@
-
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
-print("Hello World! Pass this test, please.\n")
+print("Hello World! Pass this test, please.")
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
